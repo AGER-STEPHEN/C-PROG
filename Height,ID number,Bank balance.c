a9aa5c1540647417396e7629e6f8ad381f1029fb
AGER-STEPHEN <agerstephen303@gmail.com>
2025-09-24 22:01:18 +0300

    Add files via upload

---
+++ Height,ID number,Bank balance.c
@@ -0,0 +1,24 @@
+//
+#include<stdio.h>
+    
+//mainfunction 
+int main()
+{
+   int height;
+   int ID_number;
+   int bank_balance;
+   
+   printf("Enter your height(in metres)");
+   scanf("%d",&height);
+   printf("your height is%d",height);
+   
+   printf("Enter your ID_number");
+   scanf("%d", &ID_number);
+   printf("your ID_number is %d",ID_number);
+   
+   printf("Enter your bank_balance");
+   scanf("%d",&bank_balance);
+   printf("your bank_balance is %d",bank_balance);
+   return 0;
+   }
+   
---
+++ Semester.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+//mainfunction
+int main(){
+    //
+    char *units[]={
+    "Discreete mathematics",
+    "Mathematics for science",
+    "Programming and problem solving",
+    "Communication skills",
+    "System analysis and design",
+    "Fundamentals of ICT",
+    "Computer systems and organization",
+    };
+    //
+    printf("units for this semester:\n");
+    
+    return 0;
+ }   
